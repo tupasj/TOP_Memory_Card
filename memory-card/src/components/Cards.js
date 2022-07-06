b2f8ee042bd45f8f7b432364529c3e4781987519
Jeremy Tupas <jeremytupas@gmail.com>
2022-07-06 11:41:54 -0500

    Updated img src to use jsDelivr CDN

--- memory-card/src/components/Cards.js
+++ memory-card/src/components/Cards.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import cardSets from "../assets/cardSets.json";
 
 const Cards = () => {
-
-}
+  return (
+    <div className="card-container">
+      <img src={cardSets.set1[0].src} alt={cardSets.set1[0].alt}></img>
+    </div>
+  );
+};
 
 export default Cards;
